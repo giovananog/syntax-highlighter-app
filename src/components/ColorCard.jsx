4b4ec724ccb1c1da9057990f08278ef0881bd843
Giovana Nogueira <ongiovana2016@gmail.com>
2024-01-29 16:25:38 -0300

    feat: color palette page style started

---
+++ src/components/ColorCard.jsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+export default function ColorCard(props) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          style={{ backgroundColor: `${props.color}`, height: 100 }}
+          alt="pallete color"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div" align='center'>
+            {props.title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {props.color}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
--- src/components/Palette.jsx
+++ src/components/Palette.jsx
@@ -1,12 +1,9 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import { IconButton } from '@mui/material';
 import ZoomInIcon from '@mui/icons-material/ZoomIn';
-import ZoomOutIcon from '@mui/icons-material/ZoomOut';
+import ColorCard from './ColorCard';
 
 
 export default function Palette() {
@@ -15,11 +12,22 @@ export default function Palette() {
       <Typography variant="h6" gutterBottom>
         Palette Color
       </Typography>
-      <Grid container spacing={3}>
-        
-        
-        
-        
+      <Grid container spacing={3} justifyContent="center" >
+        <Grid item xs={3.5}>
+            <ColorCard title='c' color='#ccc'/>
+        </Grid>
+        <Grid item xs={3.5}>
+            <ColorCard title='c' color='#ccc'/>
+        </Grid>
+        <Grid item xs={3.5}>
+            <ColorCard title='c' color='#ccc'/>
+        </Grid>
+        <Grid item xs={3.5}>
+            <ColorCard title='c' color='#ccc'/>
+        </Grid>
+        <Grid item xs={3.5}>
+            <ColorCard title='c' color='#ccc'/>
+        </Grid>
         <Grid item xs={12} container justifyContent="flex-start">
             <IconButton aria-label="zoom-in" color="primary">
               <ZoomInIcon />
