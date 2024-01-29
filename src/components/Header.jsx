0ab30825b174046099e58bafb014536969ce0cd3
Giovana Nogueira <ongiovana2016@gmail.com>
2024-01-29 12:07:53 -0300

    feat: header style started

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Switch from '@mui/material/Switch';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormGroup from '@mui/material/FormGroup';
 
-function Header() {
+export default function Header() {
+  const [mode, setMode] = React.useState(true);
+  
+  const handleChange = (event) => {
+    setMode(event.target.checked);
+  };
+
   return (
-    <h1>Syntax Highlighter</h1>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{ backgroundColor: mode ? '#1a1a1a' : '#ffffff' }}>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Code Syntax Highlighter
+          </Typography>
+          <FormGroup>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={mode}
+              onChange={handleChange}
+              aria-label="login switch"
+            />
+          }
+          label={mode ? 'dark mode' : 'light mode'}
+        />
+      </FormGroup>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
-
-export default Header;
