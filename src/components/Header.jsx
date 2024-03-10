6ffa80f32870cfb43ca618d3360d7d3267f26819
Giovana Nogueira <ongiovana2016@gmail.com>
2024-03-10 14:54:47 -0300

    feat: update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,8 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Switch from '@mui/material/Switch';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormGroup from '@mui/material/FormGroup';
 
 export default function Header() {
-  const [mode, setMode] = React.useState(true);
-  
-  const handleChange = (event) => {
-    setMode(event.target.checked);
-  };
-
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar
@@ -29,18 +20,6 @@ export default function Header() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Code Syntax Highlighter
           </Typography>
-          <FormGroup>
-        <FormControlLabel
-          control={
-            <Switch
-              checked={mode}
-              onChange={handleChange}
-              aria-label="login switch"
-            />
-          }
-          label={mode ? 'dark mode' : 'light mode'}
-        />
-      </FormGroup>
         </Toolbar>
       </AppBar>
     </Box>
